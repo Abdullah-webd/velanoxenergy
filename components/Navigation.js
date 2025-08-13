2add72047837b15ff6cd5564f02b89251654704b
WebMaster <ajalabdllh@gmail.com>
2025-08-13 12:52:34 +0100

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -28,9 +28,6 @@ export default function Navigation() {
             <Link href="/contact" className="text-gray-700 hover:text-blue-600 transition-colors">
               Contact
             </Link>
-            <Link href="/ceopage" className="text-gray-700 hover:text-blue-600 transition-colors">
-              Our CEO
-            </Link>
           </div>
 
           <div className="md:hidden flex items-center">
